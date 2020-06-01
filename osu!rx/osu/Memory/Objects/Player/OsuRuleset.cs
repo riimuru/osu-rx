a1c10611466142c767ce572fe2bde4b71019626e
mrflashstudio <mrflashstudio@gmail.com>
2020-06-01 21:45:15 +0300

    Fix MousePosition not working for osu!catch ruleset

--- osu!rx/osu/Memory/Objects/Player/OsuRuleset.cs
+++ osu!rx/osu/Memory/Objects/Player/OsuRuleset.cs
@@ -9,20 +9,23 @@ namespace osu_rx.osu.Memory.Objects.Player
         {
             get
             {
+                UIntPtr catcherAddress = (UIntPtr)OsuProcess.ReadUInt32(BaseAddress + 0x8C);
+
                 bool isCtb = OsuProcess.ReadFloat(BaseAddress + 0x50) == 340;
-                float x = OsuProcess.ReadFloat(BaseAddress + (isCtb ? 0x4C : 0x7C));
-                float y = OsuProcess.ReadFloat(BaseAddress + (isCtb ? 0x50 : 0x80));
+                float x = OsuProcess.ReadFloat(isCtb ? catcherAddress + 0x4C : BaseAddress + 0x7C);
+                float y = OsuProcess.ReadFloat(isCtb ? catcherAddress + 0x50 : BaseAddress + 0x80);
 
                 return new Vector2(x, y);
             }
             set
             {
-                bool isCtb = OsuProcess.ReadFloat(BaseAddress + 0x50) == 340;
+                UIntPtr catcherAddress = (UIntPtr)OsuProcess.ReadUInt32(BaseAddress + 0x8C);
+
+                bool isCtb = OsuProcess.ReadFloat(catcherAddress + 0x50) == 340;
                 if (!isCtb)
                     return;
 
                 UIntPtr wank = (UIntPtr)OsuProcess.ReadUInt32(BaseAddress + 0xA4);
-                UIntPtr catcherAddress = (UIntPtr)OsuProcess.ReadUInt32(BaseAddress + 0x8C);
 
                 OsuProcess.WriteMemory(wank + 0x8, BitConverter.GetBytes(value.X), sizeof(float));
                 OsuProcess.WriteMemory(wank + 0xC, BitConverter.GetBytes(1), sizeof(int));
